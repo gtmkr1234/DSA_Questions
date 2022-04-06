9eb21255fa6b535480c218c2bf26dfd3e3b786b0
pc <gtmkr1234@gmail.com>
2022-04-07 01:19:56 +0530

    Reverse an array iteratively

--- src/Array/ReverseArray.java
+++ src/Array/ReverseArray.java
@@ -18,12 +18,31 @@ public class ReverseArray {
             System.out.println(res);
         }
     }
+    public static int[] reverse(int [] arr){
+        int temp;
+        int start = 0;
+        int end = arr.length-1;
+        while(start<end){
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        return arr;
+    }
 
 }
 class exe{
     public static void main(String[] args) {
-        ReverseArray o1 = new ReverseArray();
-        int [] arr = createArray(4);
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the size of the array :");
+        int size = s.nextInt();
+        int [] arr = createArray(size);
+        System.out.println("Original Array :");
+        print(arr);
+        reverse(arr);
+        System.out.println("Reversed Array :");
         print(arr);
     }
 }
